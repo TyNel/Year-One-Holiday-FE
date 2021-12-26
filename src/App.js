a64c7e193aa8f9a271c6e0d398161e5d9255cb90
tyler nelson <TyNel94@outlook.com>
2021-12-26 11:50:44 -0800

    added styling and code for sign-in and sign-up pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SignIn from "./pages/sign-in/sign-in.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
 }
 
 export default App;
